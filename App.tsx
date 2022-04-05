79a3b43653b58852e2b71210f9d02e5410dfbe0a
eabra <emab21@student.bth.se>
2022-04-05 03:41:47 +0200

    added new products and style

--- App.tsx
+++ App.tsx
@@ -5,28 +5,27 @@ import icecream from './assets/icecream.jpg';
 import Stock from './components/Stock.tsx';
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.base}>
-        <Text style={{color: '#32CD32', fontSize: 42, textAlign: 'center'}}>Lager-Appen</Text>
-        <Image source={icecream} style={{ width: 320, height: 240, marginLeft: 12 }} />
-        <Stock />
-        <StatusBar style="auto" />
-      </ScrollView>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.base}>
+                <Text style={{color: '#32CD32', fontSize: 42, textAlign: 'center'}}>Lager-Appen</Text>
+                <Image source={icecream} style={{ width: 320, height: 240, marginLeft: 12 }} />
+                <Stock />
+                <StatusBar style="auto" />
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  base: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingLeft: 12,
-    paddingRight: 12,
-    backgroundColor: '#FFC0CB'
-
-  }
+    container: {
+        flex: 1
+    },
+    base: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingLeft: 12,
+        paddingRight: 12,
+        backgroundColor: '#FFC0CB'
+    }
 });
--- components/Stock.tsx
+++ components/Stock.tsx
@@ -3,45 +3,44 @@ import { Text, View, StyleSheet } from 'react-native';
 import config from "../config/config.json";
 
 function StockList() {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    fetch(`${config.base_url}/products?api_key=${config.api_key}`)
-      .then(response => response.json())
-      .then(result => setProducts(result.data));
-  }, []);
+    useEffect(() => {
+        fetch(`${config.base_url}/products?api_key=${config.api_key}`)
+        .then(response => response.json())
+        .then(result => setProducts(result.data));
+    }, []);
 
-  const list = products.map((product, index) => <Text style={ styles.list } key={index}>{ product.name } - { product.stock }</Text>);
-  // console.log(list);
+    const list = products.map((product, index) => <Text style={ styles.list } key={index}>{ product.name } - { product.stock }</Text>);
 
-  return (
-    <View>
-      {list}
-    </View>
-  );
+    return (
+        <View>
+            {list}
+        </View>
+    );
 }
 
 export default function Stock() {
-  return (
-    <View>
-      <Text style={styles.header}>Lagerförteckning</Text>
-      <StockList/>
-    </View>
-  );
+    return (
+        <View>
+            <Text style={styles.header}>Lagerförteckning</Text>
+            <StockList/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    color: '#333',
-    fontSize: 24,
-    paddingLeft: 12,
-    marginTop: 0,
-    marginBottom: 10
-  },
-  list: {
-    paddingLeft: 12,
-    fontSize: 17,
-    marginBottom: 10,
-    color: '#444'
-  }
+    header: {
+        color: '#333',
+        fontSize: 24,
+        paddingLeft: 12,
+        marginTop: 0,
+        marginBottom: 10
+    },
+    list: {
+        paddingLeft: 12,
+        fontSize: 17,
+        marginBottom: 10,
+        color: '#444'
+    }
 });
