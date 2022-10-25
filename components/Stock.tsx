5755e2db1b42219f25db0989ae10633cae2bb58f
eabra <emab21@student.bth.se>
2022-10-25 16:36:20 +0200

    separate Stock to work with test

--- components/Stock.tsx
+++ components/Stock.tsx
@@ -2,29 +2,7 @@ import { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { Typography } from '../styles';
 import productsModel from "../models/products.ts";
-
-
-function StockList({products, setProducts}) {
-    useEffect(() => {
-        (async () => {
-            setProducts(await productsModel.getProducts());
-        })();
-    }, []);
-
-    const list = products.map((product, index) => {
-        return <Text
-                style={Typography.normal}
-                key={index}>
-                    { product.name } - { product.stock }
-                </Text>
-    });
-
-    return (
-        <View>
-            {list}
-        </View>
-    );
-}
+import StockList from "./StockList.tsx";
 
 export default function Stock({products, setProducts}) {
     return (
